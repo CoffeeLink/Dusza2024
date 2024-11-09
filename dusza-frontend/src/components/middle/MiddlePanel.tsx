6c3ef356a5320c88e822d9f9f4398c292e70666f
Zente Hollo Cegledi <czh@czh.hu>
2024-11-09 13:02:08 +0100

    Update main page

--- dusza-frontend/src/components/middle/MiddlePanel.tsx
+++ dusza-frontend/src/components/middle/MiddlePanel.tsx
@@ -7,24 +7,25 @@ export const MiddlePanel = ({
   rightButton,
   children,
 }: {
-  title: string;
+  title?: string;
   leftButton?: React.ReactNode;
   rightButton?: React.ReactNode;
   children: React.ReactNode;
 }) => {
   return (
-    <div className="w-full flex flex-col gap-2">
+    <div className="w-full flex flex-col gap-2 p-20 flex flex-col gap-6 justify-center items-center p-6 bg-slate-50 border border-1 rounded-lg border-slate-500 shadow-lg">
       <div className="relative">
         {leftButton && (
           <div className="absolute -top-2 left-0">{leftButton}</div>
         )}
-        <h1 className="w-full text-center text-4xl">{title}</h1>
+        <h1 className="w-full text-center text-4xl font-bold text-gray-800">{title}</h1>
+        
         {rightButton && (
           <div className="absolute -top-2 right-0">{rightButton}</div>
         )}
       </div>
-
-      <Artboard className="w-full bg-white p-2">{children}</Artboard>
+      
+      <Artboard className="bg-transparent shadow-none">{children}</Artboard>
     </div>
   );
 };
