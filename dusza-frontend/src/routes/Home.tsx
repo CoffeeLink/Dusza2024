7f1371690da6da5df6d2d554e976d3e3649b6a81
Zoli06 <zolixvagyok@gmail.com>
2024-11-09 12:23:49 +0100

    Home use MiddlePanel

--- dusza-frontend/src/routes/Home.tsx
+++ dusza-frontend/src/routes/Home.tsx
@@ -1,18 +1,40 @@
+import { MiddlePanel } from "../components/middle/MiddlePanel.tsx";
+
 export const Home = () => {
   return (
-    <div className="p-20 flex flex-col gap-6 justify-center items-center p-6 bg-slate-50 border border-1 rounded-lg border-slate-500 shadow-lg">
-      <h1 className="text-2xl text-center">
-        Üdvözlünk a <br />
-        <span className="text-4xl font-bold text-amber-600">Dusza Árpád Programozási Emlékverseny</span><br />
-        <div className="p-1">jelentkezési felületén!</div>
-      </h1>
-      <p className="text-md md:text-xl text-center text-gray-700 max-w-2xl ">
-        Ezen a felületen regisztrálhatod a csapatodat a különböző <br/>Dusza Árpád Programozási versenyekre.
-      </p>
-      <p className="text-lg">
-        A kezdéshez <a href="/register" className="text-blue-500 font-bold hover:text-blue-700">regisztráld a csapatodat</a>, vagy {" "}
-        <a href="/login" className="text-blue-500 font-bold hover:text-blue-700">lépj be</a>.
-      </p>
+    // <div className="p-20 flex flex-col gap-6 justify-center items-center p-6 bg-slate-50 border border-1 rounded-lg border-slate-500 shadow-lg">
+    <div className="w-full max-w-3xl flex flex-col gap-4 justify-center">
+      <MiddlePanel title={"Home"}>
+        <h1 className="text-2xl text-center">
+          Üdvözlünk a <br />
+          <span className="text-4xl font-bold text-amber-600">
+            Dusza Árpád Programozási Emlékverseny
+          </span>
+          <br />
+          <div className="p-1">jelentkezési felületén!</div>
+        </h1>
+        <p className="text-md md:text-xl text-center text-gray-700 max-w-2xl ">
+          Ezen a felületen regisztrálhatod a csapatodat a különböző <br />
+          Dusza Árpád Programozási versenyekre.
+        </p>
+        <p className="text-lg">
+          A kezdéshez{" "}
+          <a
+            href="/register"
+            className="text-blue-500 font-bold hover:text-blue-700"
+          >
+            regisztráld a csapatodat
+          </a>
+          , vagy{" "}
+          <a
+            href="/login"
+            className="text-blue-500 font-bold hover:text-blue-700"
+          >
+            lépj be
+          </a>
+          .
+        </p>
+      </MiddlePanel>
     </div>
   );
 };
