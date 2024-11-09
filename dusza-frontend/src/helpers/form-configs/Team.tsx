9b773242377c87a641d6dd936583af99f720bde7
Zente Hollo Cegledi <czh@czh.hu>
2024-11-09 14:12:30 +0100

    Add icons, fonts, update the main page and login page

--- dusza-frontend/src/helpers/form-configs/Team.tsx
+++ dusza-frontend/src/helpers/form-configs/Team.tsx
@@ -14,7 +14,7 @@ export const GetLoginConfig: GetConfig<LoginFields> = (onChange, fields) => {
   const config: Config[] = [
     {
       key: "username",
-      label: "Username",
+      label: "Felhasználónév",
       errorFlag: false,
       errorMsg: "",
       value: fields.username,
@@ -24,7 +24,7 @@ export const GetLoginConfig: GetConfig<LoginFields> = (onChange, fields) => {
     },
     {
       key: "password",
-      label: "Password",
+      label: "Jelszó",
       errorFlag: false,
       errorMsg: "A jelszó túl rövid!",
       value: fields.password,
