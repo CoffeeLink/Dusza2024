def74971555ac999abb05229aee5038d745ff15a
Áron Demeter <demeter.aron0614@gmail.com>
2024-11-10 12:04:13 +0100

    Updated User Model tools

--- dusza_backend/src/models/user.rs
+++ dusza_backend/src/models/user.rs
@@ -1,9 +1,10 @@
 use log::error;
 use crate::models::user::UserType::{SchoolRepresentative, TeamAccount};
 use serde::{Deserialize, Serialize};
-use sqlx::{query_as, Decode, Encode, FromRow, MySql, Type};
+use sqlx::{query, query_as, Decode, Encode, FromRow, MySql, Type};
 use uuid::Uuid;
-use crate::Database;
+use crate::{AuthConfig, Database};
+use crate::auth::salt;
 use crate::error::DuszaBackendError;
 use crate::models::DBError;
 
@@ -89,6 +90,63 @@ impl User {
             })?;
 
         Ok(user)
+    }
+
+    pub async fn create_user(
+        username: impl Into<String>,
+        password: impl Into<String>,
+        user_type: UserType,
+        db: &Database,
+        auth_config: &AuthConfig
+    ) -> Result<User, DuszaBackendError<DBError>> {
+        let salted_password = salt(password.into(), auth_config);
+        let sql = r#"
+INSERT INTO user (username, password, user_type)
+VALUE (?, ?, ?)
+        "#;
+        let usr_name = username.into();
+        let user_id: UserId = query(sql)
+            .bind(usr_name.clone())
+            .bind(salted_password)
+            .bind(user_type.clone())
+            .execute(db)
+            .await
+            .map_err(|e| {
+                error!("MYSQL EXEC ERROR: {e}");
+                DuszaBackendError::InternalError
+            })?
+            .last_insert_id() as u32;
+
+        Ok(User{
+            user_id,
+            username: usr_name,
+            user_type
+        })
+    }
+
+    pub async fn change_user_password(
+        user_id: UserId,
+        new_password: impl Into<String>,
+        db: &Database,
+        auth_config: &AuthConfig
+    ) -> Result<(), DuszaBackendError<DBError>> {
+        let sql = r#"
+UPDATE user
+SET password = ?
+WHERE user_id = ?
+        "#;
+        let new_password = salt(new_password.into(), auth_config);
+        let _ = query(sql)
+            .bind(new_password)
+            .bind(user_id)
+            .execute(db)
+            .await
+            .map_err(|e| {
+                error!("MYSQL EXEC ERROR: {e}");
+                DuszaBackendError::InternalError
+            })?;
+
+        Ok(())
     }
 }
 
